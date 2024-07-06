24526f3b9a4788dfb15596fd3d0bfcf5f0bed366
axtrough <lolmyfriend14@gmail.com>
2024-07-06 07:28:20 +0200

    Initial commit

---
+++ src/main/java/net/raccoons/will/willsworkshop/WWClient.java
@@ -0,0 +1,10 @@
+package net.raccoons.will.willsworkshop;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WWClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/raccoons/will/willsworkshop/WillsWorkshop.java
@@ -0,0 +1,16 @@
+package net.raccoons.will.willsworkshop;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WillsWorkshop implements ModInitializer {
+	public static final String MODID = "willsworkshop";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
